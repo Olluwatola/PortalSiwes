f59f47053fe3b4c4b67c2a903f9be090ab28e011
Iyanu <iyanuxn@gmail.com>
2024-01-13 19:32:54 +0100

    Animation to about

--- src/pages/mainAppPages/About.jsx
+++ src/pages/mainAppPages/About.jsx
@@ -5,6 +5,7 @@ import {
   getDocs,
 } from "firebase/firestore";
 import { db } from "./../../config/firebase";
+import { motion } from "framer-motion";
 
 function About() {
   const [about, setAbout] = useState();
@@ -25,15 +26,23 @@ function About() {
     getAbout();
   }, [about]);
   return (
-    <div className="md:py-36 py-20 gap-5 md:gap-7 md:px-16 px-6 flex flex-col ">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      className="md:py-36 py-20 gap-5 md:gap-7 md:px-16 px-6 flex flex-col "
+    >
       <div className="flex flex-col gap-2 md:gap-4 text-darkBlue">
-        <span className="font-medium text-4xl md:text-6xl -tracking-widest">About</span>
+        <span className="font-medium text-4xl md:text-6xl -tracking-widest">
+          About
+        </span>
         <span className="tracking-tighter">Get to know us</span>
       </div>
       <div className="md:w-3/4 text-justify md:text-base text-sm leading-relaxed md:leading-[1.7rem] font-light">
         {about ? about : ""}
       </div>
-    </div>
+    </motion.div>
   );
 }
 
