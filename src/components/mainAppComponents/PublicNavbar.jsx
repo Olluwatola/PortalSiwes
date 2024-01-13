5afe71ba9800b6310879c5bc9e5d4c57ecd598cd
Iyanu <iyanuxn@gmail.com>
2024-01-13 21:31:17 +0100

    Contact us and navbar update

--- src/components/mainAppComponents/PublicNavbar.jsx
+++ src/components/mainAppComponents/PublicNavbar.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 const PublicNavbar = ({ applyModal, setapplyModal }) => {
   const [home, setHome] = useState(null);
+  const [active, setActive] = useState();
   const location = useLocation();
 
   const handleHome = () => {
@@ -13,8 +14,19 @@ const PublicNavbar = ({ applyModal, setapplyModal }) => {
     }
   };
 
+  const handleActive = () => {
+    if (location.pathname === "/about") {
+      setActive("about");
+    } else if (location.pathname === "/faqs") {
+      setActive("faqs");
+    } else if (location.pathname === "/contactus") {
+      setActive("contactus");
+    }
+  };
+
   useEffect(() => {
     handleHome();
+    handleActive();
   }, [location.pathname]);
 
   return (
@@ -28,7 +40,7 @@ const PublicNavbar = ({ applyModal, setapplyModal }) => {
         className={`
       ${home ? "text-white" : "text-slate-500"}
       text-sm md:text-base flex items-center justify-between md:justify-start md:w-fit gap-3
-      transition-all ease-in-out duration-300
+      transition-all ease-in-out duration-300 font-medium
       `}
       >
         <li
@@ -58,25 +70,45 @@ const PublicNavbar = ({ applyModal, setapplyModal }) => {
           )}
         </li>
         <span className="text-2xl font-light md:flex hidden">|</span>
-        <li className="hidden md:flex">
+        <li
+          className={`
+        ${active === "about" ? "text-black" : ""}
+        hidden md:flex transition-all ease-in-out duration-200`}
+        >
           <Link to="/about">About</Link>
         </li>
-        <li className="hidden md:flex">
+        <li
+          className={`${active === "faqs" ? "text-black" : ""}
+          hidden md:flex transition-all ease-in-out duration-200`}
+        >
           <Link to="/faqs">FAQs</Link>
         </li>
-        <li className="hidden md:flex">
+        <li
+          className={`${active === "contactus" ? "text-black" : ""}
+          hidden md:flex transition-all ease-in-out duration-200`}
+        >
           <Link to="/contactus">Contact Us</Link>
         </li>
 
         {/* For mobile */}
         <div className="flex gap-4 md:hidden">
-          <li>
+          <li
+            className={`
+          ${active === "about" ? "text-black" : ""}
+          transition-all ease-in-out duration-200`}
+          >
             <Link to="/about">About</Link>
           </li>
-          <li>
+          <li
+            className={`${active === "faqs" ? "text-black" : ""}
+          transition-all ease-in-out duration-200`}
+          >
             <Link to="/faqs">FAQs</Link>
           </li>
-          <li>
+          <li
+            className={`${active === "contactus" ? "text-black" : ""}
+          transition-all ease-in-out duration-200`}
+          >
             <Link to="/contactus">Contact Us</Link>
           </li>
         </div>
--- src/pages/mainAppPages/ContactUs.jsx
+++ src/pages/mainAppPages/ContactUs.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { db } from "./../../config/firebase";
 import ContactUsForm from "./../../components/mainAppComponents/ContactUsForm";
 import { collection, getDocs } from "firebase/firestore";
+import { motion } from "framer-motion";
 
 function ContactUs() {
   const [contactUsInfo, setContactUsInfo] = useState([]);
@@ -23,7 +24,13 @@ function ContactUs() {
   }, []);
 
   return (
-    <div className="py-36 gap-7 px-16 flex flex-col ">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      className="md:py-36 py-20 gap-5 md:gap-7 md:px-16 px-6 flex flex-col "
+    >
       <div className="flex flex-col gap-4 text-darkBlue">
         <span className="font-medium text-6xl -tracking-widest">
           Contact Us
@@ -39,7 +46,7 @@ function ContactUs() {
       <br />
       {contactUsInfo[0]?.address}
       {/* <ContactUsForm /> */}
-    </div>
+    </motion.div>
   );
 }
 export default ContactUs;
