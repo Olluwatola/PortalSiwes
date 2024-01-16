330d43135154029cd3707a8b0524e595bcc05641
Iyanu <iyanuxn@gmail.com>
2024-01-16 09:13:45 +0100

    Added splash screen

--- src/pages/adminPages/Auth.jsx
+++ src/pages/adminPages/Auth.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LoginForm from "./../../components/adminComponents/LoginForm";
 import StatusBar from "../../components/adminComponents/adminStatusBar/AdminStatusBar";
+import SplashScreen from "../../components/adminComponents/SplashScreen";
+import { AnimatePresence } from "framer-motion";
+
 function Admin() {
   const [conditionGood, setConditionGood] = useState(null);
   const [statusBarMessage, setStatusBarMessage] = useState(null);
+  const [splashScreen, setSplashScreen] = useState(true);
+
+  const hideSplashScreen = () => {
+    setTimeout(() => {
+      setSplashScreen(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    hideSplashScreen();
+  }, []);
+
   return (
     <>
+      <AnimatePresence>{splashScreen && <SplashScreen />}</AnimatePresence>
       <StatusBar
         conditionGood={conditionGood}
         statusBarMessage={statusBarMessage}
