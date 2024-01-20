b57e2a6035c66d0f2d3e6918b1bd7fb9bb9b3769
Iyanu <iyanuxn@gmail.com>
2024-01-20 04:40:13 +0100

    dashbaoar navbar setup

--- src/components/adminComponents/AdminNavbar.jsx
+++ src/components/adminComponents/AdminNavbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { signUserOut } from "./../../controllers/authControllers";
-import { RxDashboard } from "react-icons/rx";
+import { RxDashboard, RxExit } from "react-icons/rx";
 import { FaWpforms } from "react-icons/fa";
 import { PiTentLight } from "react-icons/pi";
 import { VscOpenPreview } from "react-icons/vsc";
@@ -31,7 +31,7 @@ const AdminNavbar = () => {
   }, [location.pathname]);
 
   return (
-    <nav className="w-72 bg-white border-r-2 border-neutral-200 flex flex-col gap-8">
+    <nav className="w-72 bg-white border-r-2 border-neutral-200 flex flex-col justify-between h-screen pb-10">
       <div className="flex flex-col gap-2">
         <span className="font-secondary flex items-center text-primary pt-10 text-3xl tracking-tight">
           IT<div className="font-primary -rotate-45 font-semibold">e</div>
@@ -47,10 +47,10 @@ const AdminNavbar = () => {
           <div
             className={`${
               active === ""
-                ? "text-primary before:translate-x-[2.2rem]  before:bg-primary bg-neutral-100"
+                ? "text-primary before:translate-x-[2.2rem] bg-neutral-100 rounded-s-none"
                 : "before:translate-x-14"
             }
-            flex gap-2 items-center h-12 hover:bg-neutral-100 transition-all duration-200 ease-in-out rounded-s-xl px-3 before:h-12 before:rounded-s-xl before:w-8 before:absolute before:-z-10 before:left-0 hover:before:translate-x-[2.2rem] before:transition-all before:duration-200 before:ease-in-out`}
+            flex gap-2 items-center h-12 hover:bg-neutral-100  before:bg-primary transition-all duration-200 ease-in-out rounded-s-xl px-3 before:h-12 before:rounded-s-xl before:w-24 before:absolute before:-z-10 before:left-0  before:transition-all before:duration-300 before:ease-in-out`}
           >
             <RxDashboard className="text-xl" />
             Dashboard
@@ -60,10 +60,10 @@ const AdminNavbar = () => {
           <div
             className={`${
               active === "applications"
-                ? "text-primary before:translate-x-[2.2rem]  before:bg-primary bg-neutral-100"
+                ? "text-primary before:translate-x-[2.2rem] bg-neutral-100"
                 : "before:translate-x-14"
             }
-            flex gap-2 items-center h-12 hover:bg-neutral-100 transition-all duration-200 ease-in-out rounded-s-xl px-3 before:h-12 before:rounded-s-xl before:w-8 before:absolute before:-z-10 before:left-0 hover:before:translate-x-[2.2rem] before:transition-all before:duration-200 before:ease-in-out`}
+            flex gap-2 items-center h-12 hover:bg-neutral-100 before:bg-primary transition-all duration-200 ease-in-out rounded-s-xl px-3 before:h-12 before:rounded-s-xl before:w-8 before:absolute before:-z-10 before:left-0 before:transition-all before:duration-300 before:ease-in-out`}
           >
             <FaWpforms className="text-xl" />
             Applications
@@ -73,10 +73,10 @@ const AdminNavbar = () => {
           <div
             className={`${
               active === "placement"
-                ? "text-primary before:translate-x-[2.2rem]  before:bg-primary bg-neutral-100"
+                ? "text-primary before:translate-x-[2.2rem] bg-neutral-100"
                 : "before:translate-x-14"
             }
-            flex gap-2 items-center h-12 hover:bg-neutral-100 transition-all duration-200 ease-in-out rounded-s-xl px-3 before:h-12 before:rounded-s-xl before:w-8 before:absolute before:-z-10 before:left-0 hover:before:translate-x-[2.2rem] before:transition-all before:duration-200 before:ease-in-out`}
+            flex gap-2 items-center h-12 hover:bg-neutral-100 before:bg-primary transition-all duration-200 ease-in-out rounded-s-xl px-3 before:h-12 before:rounded-s-xl before:w-8 before:absolute before:-z-10 before:left-0 before:transition-all before:duration-300 before:ease-in-out`}
           >
             <PiTentLight className="text-2xl" />
             Placement Posting
@@ -86,10 +86,10 @@ const AdminNavbar = () => {
           <div
             className={`${
               active === "screening"
-                ? "text-primary before:translate-x-[2.2rem]  before:bg-primary bg-neutral-100"
+                ? "text-primary before:translate-x-[2.2rem] bg-neutral-100"
                 : "before:translate-x-14"
             }
-            flex gap-2 items-center h-12 hover:bg-neutral-100 transition-all duration-200 ease-in-out rounded-s-xl px-3 before:h-12 before:rounded-s-xl before:w-8 before:absolute before:-z-10 before:left-0 hover:before:translate-x-[2.2rem] before:transition-all before:duration-200 before:ease-in-out`}
+            flex gap-2 items-center h-12 hover:bg-neutral-100 before:bg-primary transition-all duration-200 ease-in-out rounded-s-xl px-3 before:h-12 before:rounded-s-xl before:w-8 before:absolute before:-z-10 before:left-0 before:transition-all before:duration-300 before:ease-in-out`}
           >
             <VscOpenPreview className="text-xl" />
             Screening
@@ -99,20 +99,24 @@ const AdminNavbar = () => {
           <div
             className={`${
               active === "portal"
-                ? "text-primary before:translate-x-[2.2rem]  before:bg-primary bg-neutral-100"
+                ? "text-primary before:translate-x-[2.2rem] bg-neutral-100"
                 : "before:translate-x-14"
             }
-            flex gap-2 items-center h-12 hover:bg-neutral-100 transition-all duration-200 ease-in-out rounded-s-xl px-3 before:h-12 before:rounded-s-xl before:w-8 before:absolute before:-z-10 before:left-0 hover:before:translate-x-[2.2rem] before:transition-all before:duration-200 before:ease-in-out`}
+            flex gap-2 items-center h-12 hover:bg-neutral-100 before:bg-primary transition-all duration-200 ease-in-out rounded-s-xl px-3 before:h-12 before:rounded-s-xl before:w-8 before:absolute before:-z-10 before:left-0 before:transition-all before:duration-300 before:ease-in-out`}
           >
             <IoSettingsOutline className="text-xl" />
             Portal Settings
           </div>
         </Link>
-        <button className="w-fit" onClick={signUserOut}>
-          logout
-        </button>
       </div>
-      </nav>
+      <button
+        className="w-full text-left text-dashBlue flex gap-2 items-center h-12 hover:bg-neutral-100 transition-all duration-200 ease-in-out rounded-s-xl px-3"
+        onClick={signUserOut}
+      >
+        <RxExit className="text-xl" />
+        Logout
+      </button>
+    </nav>
   );
 };
 
