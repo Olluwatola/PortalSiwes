db229e234a5a6a3f1499e13f7c54a96a3b30ddbf
Iyanu <iyanuxn@gmail.com>
2024-01-20 01:19:19 +0100

    404 page

--- src/pages/mainAppPages/error404.jsx
+++ src/pages/mainAppPages/error404.jsx
@@ -6,13 +6,19 @@ const error404 = () => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.1 }}
-      className="flex flex-col items-center justify-center h-[85vh]"
+      transition={{ duration: 0.5 }}
+      className="flex flex-col items-center justify-center h-[85vh] bg"
     >
-      <span className="font-black text-9xl text-primary">404</span>
+      <span className="font-black text-6xl md:text-9xl text-primary">404</span>
       <span className="text-sm font-medium text-neutral-700">
         Sorry, Page not found
       </span>
+      <button
+        className="bg-primary py-2 md:mt-7 mt-5 px-3 md:px-5 rounded-lg text-xs md:text-sm transition-all duration-300 ease-in-out  text-white  hover:bg-white hover:text-primary hover:border-primary border border-primary"
+        onClick={() => window.location.replace("/")}
+      >
+        Go back home
+      </button>
     </motion.div>
   );
 };
