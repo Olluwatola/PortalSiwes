a9569f6470154a6648ff68390ceb22da58214e65
Iyanu <iyanuxn@gmail.com>
2024-02-11 22:36:25 +0100

    pagination logic setup

--- src/components/adminComponents/adminListItem/Pagination.jsx
+++ src/components/adminComponents/adminListItem/Pagination.jsx
@@ -1,48 +1,68 @@
+import { FiChevronLeft, FiChevronRight, FiChevronsLeft, FiChevronsRight } from "react-icons/fi";
+
 const Pagination = ({
-    totalPages,
-    currentPage,
-    paginate,
-  }) => {
-    // Function to generate an array of page numbers
-    const getPageNumbers = () => {
-      const pageNumbers = [];
-      let startingPage = Math.max(1, currentPage - 1); // Ensure starting page is not less than 1
-      if (currentPage >= totalPages - 2) {
-        startingPage = Math.max(1, totalPages - 2);
-      }
-      for (let i = startingPage; i <= Math.min(startingPage + 2, totalPages); i++) {
-        pageNumbers.push(i);
-      }
-      return pageNumbers;
-    };
-  
-    return (
-      <ul className="flex mt-8">
-        {getPageNumbers().map((page) => (
-          <li key={page} className="mx-1">
-            <button
-              onClick={() => paginate(page)}
-              className={`h-8 w-8 flex justify-center items-center rounded-md ${
-                currentPage === page ? "bg-primary text-white" : "bg-neutral-200"
-              } transition-all duration-200 ease-in-out`}
-            >
-              {page}
+  totalPages,
+  currentPage,
+  paginate,
+}) => {
+  // Function to generate an array of page numbers
+  const getPageNumbers = () => {
+    const pageNumbers = [];
+    let startingPage = Math.max(1, currentPage - 1); // Ensure starting page is not less than 1
+    if (currentPage >= totalPages - 2) {
+      startingPage = Math.max(1, totalPages - 2);
+    }
+    for (let i = startingPage; i <= Math.min(startingPage + 2, totalPages); i++) {
+      pageNumbers.push(i);
+    }
+    return pageNumbers;
+  };
+
+  return (
+    <ul className="flex mt-8 text-neutral-600">
+      {totalPages > 3 && currentPage > 2 && (
+        <>
+          <li className="mx-1">
+            <button className="w-8 h-8 transition-all duration-200 ease-in-out rounded-md flex justify-center items-center hover:bg-primary hover:text-white" onClick={() => paginate(1)}>
+              <FiChevronsLeft />
             </button>
           </li>
-        ))}
-        {totalPages > 3  && (
-          <>
-            <li className="mx-1">
-              <button onClick={() => paginate(currentPage + 1)}>Next</button>
-            </li>
-            <li className="mx-1">
-              <button onClick={() => paginate(totalPages)}>Last</button>
-            </li>
-          </>
-        )}
-      </ul>
-    );
-  };
-  
-  export default Pagination;
-  
+          <li className="mx-1">
+            <button className="w-8 h-8 transition-all duration-200 ease-in-out rounded-md flex justify-center items-center hover:bg-primary hover:text-white" onClick={() => paginate(Math.max(1, currentPage - 1))}>
+              <FiChevronLeft />
+            </button>
+          </li>
+        </>
+      )}
+      {getPageNumbers().map((page) => (
+        <li key={page} className="mx-1">
+          <button
+            onClick={() => paginate(page)}
+            className={`h-8 w-8 flex justify-center items-center rounded-md hover:bg-primary hover:text-white ${
+              currentPage === page ? "bg-primary text-white" : ""
+            } transition-all duration-200 ease-in-out`}
+          >
+            {page}
+          </button>
+        </li>
+      ))}
+      {totalPages > 3 && currentPage < totalPages - 1 && (
+        <>
+          <li className="mx-1">
+            
+            <button className="w-8 h-8 transition-all duration-200 ease-in-out rounded-md flex justify-center items-center hover:bg-primary hover:text-white" onClick={() => paginate(currentPage + 1)}>
+              <FiChevronRight />
+            </button>
+          </li>
+          <li className="mx-1">
+            <button className="w-8 h-8 transition-all duration-200 ease-in-out rounded-md flex justify-center items-center hover:bg-primary hover:text-white" onClick={() => paginate(totalPages)}>
+              <FiChevronsRight />
+            </button>
+          </li>
+        </>
+      )}
+    </ul>
+  );
+};
+
+export default Pagination;
